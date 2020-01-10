98f1c50e5be24c598f494a4bdeac762fa502b172
vihong <probamaths@gmail.com>
2020-01-10 04:49:55 +0100

    :fire: Delete comments from Sales Component

---
+++ monboncoin/build/precache-manifest.31e0fc4cde0fa8d6010615d8104e7056.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c0a872895d1595c687cdfcfc3b1020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb30403ac210a97c4cc",
+    "url": "/static/css/main.f46bf0b0.chunk.css"
+  },
+  {
+    "revision": "a48e41886052546b8123",
+    "url": "/static/js/2.86a0426d.chunk.js"
+  },
+  {
+    "revision": "6dafaded9c287fa872bf146755bde0c2",
+    "url": "/static/js/2.86a0426d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "deb30403ac210a97c4cc",
+    "url": "/static/js/main.2f3343ed.chunk.js"
+  },
+  {
+    "revision": "9d7f1b3a9237d63e8b5e",
+    "url": "/static/js/runtime-main.103b3a08.js"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
